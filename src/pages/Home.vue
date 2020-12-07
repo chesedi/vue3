7ad4f90a6369dd4736799ea16008acede251730d
chesedi <a02t02@gmail.com>
2020-12-07 13:12:13 +0900

    Add Vue router

---
+++ src/pages/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="m-auto">
+    <h1 class="text-center">Welcome to Vue 3 Full Corse</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
