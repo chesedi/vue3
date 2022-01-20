d0f805dd657d569b1f1d32a8f7f8ec242a4137fa
chesedi <a02t02@gmail.com>
2022-01-20 21:02:29 +0900

    [단축키] 커밋 추가하기

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
-// const ansiRegex = require('ansi-regex');
+// const ansiRegex  = require('ansi-regex');
+// 메시지 추가 커밋 날리기
 module.exports = {
   configureWebpack: {
     // entry: ['@babel/polyfill', './src/main.js'],
